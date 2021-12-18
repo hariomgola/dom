ea419c2fdfdc6ce2079e6a5cc61330fc42822a8c
Hariom <hariom987316@gmail.com>
2021-12-19 00:57:26 +0530

    Dom notes check-in

---
+++ DOMNotes/DOM.js
@@ -0,0 +1,7 @@
+document.getElementById("root").innerHTML = "Hello I am coming from inner html";
+document.getElementById("url").innerHTML = document.URL;
+
+function changeURL() {
+  let currentURL = document.URL;
+  window.location = "¯\_(ツ)_/¯";
+}
